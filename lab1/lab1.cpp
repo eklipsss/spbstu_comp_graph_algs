b95c06c231cffda84919c348fc9850037f66b982
eklipsss <lips2003@mail.ru>
2025-02-09 03:13:03 +0300

    add lab1

---
+++ lab1/lab1.cpp
@@ -0,0 +1,323 @@
+﻿#include "framework.h"
+#include "lab1.h"
+#include <dxgi.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+#define MAX_LOADSTRING 100
+
+HINSTANCE hInst;                                
+WCHAR szTitle[MAX_LOADSTRING];                 
+WCHAR szWindowClass[MAX_LOADSTRING];            
+
+ID3D11Device* g_pd3dDevice = nullptr;
+ID3D11DeviceContext* g_pd3dDeviceContext = nullptr;
+IDXGISwapChain* g_pSwapChain = nullptr;
+ID3D11RenderTargetView* g_pRenderTargetView = nullptr;
+
+UINT WindowWidth = 1280;
+UINT WindowHeight = 720;
+
+HRESULT             InitDirectX(HWND hWnd);
+void                Render();
+void                ResizeWindow(int width, int height);
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_LAB1, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    if (!InitInstance(hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HWND hWnd = FindWindow(szWindowClass, szTitle);
+    if (FAILED(InitDirectX(hWnd)))
+    {
+        if (g_pd3dDeviceContext) g_pd3dDeviceContext->ClearState();
+
+        if (g_pRenderTargetView) g_pRenderTargetView->Release();
+        if (g_pSwapChain) g_pSwapChain->Release();
+        if (g_pd3dDeviceContext) g_pd3dDeviceContext->Release();
+        if (g_pd3dDevice) g_pd3dDevice->Release();
+
+        return FALSE;
+    }
+
+    SetWindowText(hWnd, L"Lips Ekaterina");
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_LAB1));
+
+    MSG msg;
+
+    bool exit = false;
+    while (!exit)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+            if (msg.message == WM_QUIT)
+            {
+                exit = true;
+            }
+        }
+        else
+        {
+            Render();
+        }
+    }
+
+    if (g_pd3dDeviceContext) g_pd3dDeviceContext->ClearState();
+
+    if (g_pRenderTargetView) g_pRenderTargetView->Release();
+    if (g_pSwapChain) g_pSwapChain->Release();
+    if (g_pd3dDeviceContext) g_pd3dDeviceContext->Release();
+    if (g_pd3dDevice) g_pd3dDevice->Release();
+
+    return (int)msg.wParam;
+}
+
+
+HRESULT InitDirectX(HWND hWnd)
+{
+    HRESULT result;
+
+    IDXGIFactory* pFactory = nullptr;
+    result = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)&pFactory);
+
+    IDXGIAdapter* pSelectedAdapter = NULL;
+    if (SUCCEEDED(result))
+    {
+        IDXGIAdapter* pAdapter = NULL;
+        UINT adapterIdx = 0;
+        while (SUCCEEDED(pFactory->EnumAdapters(adapterIdx, &pAdapter)))
+        {
+            DXGI_ADAPTER_DESC desc;
+            pAdapter->GetDesc(&desc);
+
+            if (wcscmp(desc.Description, L"Microsoft Basic Render Driver") != 0)
+            {
+                pSelectedAdapter = pAdapter;
+                break;
+            }
+
+            pAdapter->Release();
+
+            adapterIdx++;
+        }
+    }
+    assert(pSelectedAdapter != NULL);
+
+    D3D_FEATURE_LEVEL level;
+    D3D_FEATURE_LEVEL levels[] = { D3D_FEATURE_LEVEL_11_0 };
+
+    if (SUCCEEDED(result))
+    {
+        UINT flags = 0;
+#ifdef _DEBUG
+        flags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+        result = D3D11CreateDevice(pSelectedAdapter, D3D_DRIVER_TYPE_UNKNOWN, NULL,
+            flags, levels, 1, D3D11_SDK_VERSION, &g_pd3dDevice, &level, &g_pd3dDeviceContext);
+        assert(level == D3D_FEATURE_LEVEL_11_0);
+        assert(SUCCEEDED(result));
+    }
+
+    if (SUCCEEDED(result))
+    {
+        DXGI_SWAP_CHAIN_DESC swapChainDesc = { 0 };
+        swapChainDesc.BufferCount = 2;
+        swapChainDesc.BufferDesc.Width = WindowWidth;
+        swapChainDesc.BufferDesc.Height = WindowHeight;
+        swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+        swapChainDesc.BufferDesc.RefreshRate.Numerator = 0;
+        swapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
+        swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+        swapChainDesc.OutputWindow = hWnd;
+        swapChainDesc.SampleDesc.Count = 1;
+        swapChainDesc.SampleDesc.Quality = 0;
+        swapChainDesc.Windowed = TRUE;
+        
+        swapChainDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+        swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+        //swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD; // протяжка в момент отжатия мыши
+        swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL; // мгновенная протяжка
+        swapChainDesc.Flags = 0;
+
+        result = pFactory->CreateSwapChain(g_pd3dDevice, &swapChainDesc, &g_pSwapChain);
+        assert(SUCCEEDED(result));
+    }
+
+    if (SUCCEEDED(result))
+    {
+        ID3D11Texture2D* pBackBuffer = nullptr;
+        result = g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+        assert(SUCCEEDED(result));
+
+        if (FAILED(result))
+            return result;
+
+        result = g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_pRenderTargetView);
+        pBackBuffer->Release();
+    }
+
+    pFactory->Release();
+    pSelectedAdapter->Release();
+
+    return result;
+}
+
+
+void Render()
+{
+    g_pd3dDeviceContext->ClearState();
+
+    ID3D11RenderTargetView* views[] = { g_pRenderTargetView };
+    g_pd3dDeviceContext->OMSetRenderTargets(1, views, nullptr);
+
+    static const FLOAT BackColor[4] = { 0.4f, 0.2f, 0.5f, 1.0f };
+    g_pd3dDeviceContext->ClearRenderTargetView(g_pRenderTargetView, BackColor);
+
+    HRESULT result = g_pSwapChain->Present(0, 0);
+    assert(SUCCEEDED(result));
+}
+
+
+void ResizeWindow(int width, int height)
+{
+    if (g_pRenderTargetView)
+    {
+        g_pd3dDeviceContext->OMSetRenderTargets(0, nullptr, nullptr);
+        g_pRenderTargetView->Release();
+        g_pRenderTargetView = nullptr;
+    }
+
+    HRESULT result = g_pSwapChain->ResizeBuffers(2, width, height, DXGI_FORMAT_R8G8B8A8_UNORM, 0);
+    assert(SUCCEEDED(result));
+
+    if (SUCCEEDED(result))
+    {
+        ID3D11Texture2D* pBackBuffer = nullptr;
+        result = g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+        assert(SUCCEEDED(result));
+
+        if (SUCCEEDED(result))
+        {
+            result = g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_pRenderTargetView);
+            assert(SUCCEEDED(result));
+            pBackBuffer->Release();
+        }
+    }
+}
+
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LAB1));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = nullptr;
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_LAB1);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance; 
+
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    {
+        RECT rc;
+        rc.left = 0;
+        rc.right = WindowWidth;
+        rc.top = 0;
+        rc.bottom = WindowHeight;
+
+        AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, TRUE);
+
+        MoveWindow(hWnd, 100, 100, rc.right - rc.left, rc.bottom - rc.top, TRUE);
+    }
+
+    return TRUE;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        switch (wmId)
+        {
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+    break;
+
+    case WM_SIZE:
+        if (g_pSwapChain && wParam != SIZE_MINIMIZED)
+        {
+            RECT rc;
+            GetClientRect(hWnd, &rc);
+            ResizeWindow(rc.right - rc.left, rc.bottom - rc.top);
+            SetWindowText(hWnd, L"Lips Ekaterina - Window Resized!");
+        }
+        break;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
